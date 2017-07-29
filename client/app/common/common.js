94953b691bcbf16eac949aa4a6f8503dbb74a1ab
smanandhar11 <smananandhar@techdigitalcorp.com>
2017-07-28 23:30:26 -0500

    good progress

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -3,12 +3,14 @@ import Header from './header/header';
 import Hero from './hero/hero';
 import User from './user/user';
 import Wrapper from './wrapper/wrapper';
+import Footer from './footer/footer';
 
 let commonModule = angular.module('app.common', [
   Header,
   Hero,
   User,
-  Wrapper
+  Wrapper,
+  Footer
 ])
 
 .name;
