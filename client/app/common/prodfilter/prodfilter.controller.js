1fa09bc468345ec68f332677fbdec9d53ef226ee
smanandhar11 <smananandhar@techdigitalcorp.com>
2017-07-31 17:55:01 -0500

    filter html/css

--- client/app/common/prodfilter/prodfilter.controller.js
+++ client/app/common/prodfilter/prodfilter.controller.js
@@ -1,6 +1,30 @@
 class ProdfilterController {
   constructor() {
-    this.name = 'prodfilter';
+
+    this.colors = [
+      'Red',
+      'Blue',
+      'Green',
+      'Yellow',
+      'Pink',
+      'Black',
+      'Others'
+    ];
+
+    this.types = [
+      'Earrings',
+      'Bracelets',
+      'Necklace',
+      'Sets',
+      'Others'
+    ];
+
+    this.prices = [
+      'Rs.0 - Rs.100',
+      'Rs.100 - Rs.500',
+      'Rs.500 - Rs.1000',
+      'Rs.1000 and more'
+    ]
   }
 }
 
