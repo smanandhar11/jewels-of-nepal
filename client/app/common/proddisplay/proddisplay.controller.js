9c6881e3bcfe503ef4e003d47bbab5139a697d1b
smanandhar11 <smananandhar@techdigitalcorp.com>
2017-08-01 22:54:47 -0500

    gallery initiated

--- client/app/common/proddisplay/proddisplay.controller.js
+++ client/app/common/proddisplay/proddisplay.controller.js
@@ -8,6 +8,7 @@ class ProddisplayController {
         'priceRange':'100-500',
         'price':'Rs.250',
         'bgPosition':'bottom',
+        'nameSpace':'58px',
         'imgSrc':'../../../app/assets/images/product-images/saphire-fall.jpg'
       },
 
@@ -18,6 +19,7 @@ class ProddisplayController {
         'priceRange':'100-500',
         'price':'Rs.250',
         'bgPosition':'center',
+        'nameSpace':'58px',
         'imgSrc':'../../../app/assets/images/product-images/purple-haze.jpg'
       },
 
@@ -28,10 +30,107 @@ class ProddisplayController {
         'priceRange':'100-500',
         'price':'Rs.250',
         'bgPosition':'center',
+        'nameSpace':'50px',
         'imgSrc':'../../../app/assets/images/product-images/straw-cough.jpg'
+      },
+      {
+        'name':'Mars Burning',
+        'type':'Earring',
+        'colorCategory':'Red',
+        'priceRange':'100-500',
+        'price':'Rs.250',
+        'bgPosition':'center',
+        'nameSpace':'58px',
+        'imgSrc':'../../../app/assets/images/product-images/mars-burning.jpg'
+      },
+      {
+        'name':'Simpearl',
+        'type':'Earring',
+        'colorCategory':'Others',
+        'priceRange':'100-500',
+        'price':'Rs.250',
+        'bgPosition':'bottom',
+        'nameSpace':'64px',
+        'imgSrc':'../../../app/assets/images/product-images/simpearl.jpg'
+      },
+
+      {
+        'name':'Deep Blue Fall',
+        'type':'Earring',
+        'colorCategory':'Others',
+        'priceRange':'100-500',
+        'price':'Rs.250',
+        'bgPosition':'bottom',
+        'nameSpace':'45px',
+        'imgSrc':'../../../app/assets/images/product-images/deep-blue-fall.jpg'
+      },
+      {
+        'name':'Black Diamond',
+        'type':'Earring',
+        'colorCategory':'Others',
+        'priceRange':'100-500',
+        'price':'Rs.250',
+        'bgPosition':'bottom',
+        'nameSpace':'45px',
+        'imgSrc':'../../../app/assets/images/product-images/black-diamond.jpg'
+      },
+      {
+        'name':'Elegant Pearl',
+        'type':'Earring',
+        'colorCategory':'Others',
+        'priceRange':'100-500',
+        'price':'Rs.250',
+        'bgPosition':'bottom',
+        'nameSpace':'45px',
+        'imgSrc':'../../../app/assets/images/product-images/elegant-pearl.jpg'
+      },
+      {
+        'name':'Green Eyed Bird',
+        'type':'Earring',
+        'colorCategory':'Others',
+        'priceRange':'100-500',
+        'price':'Rs.250',
+        'bgPosition':'bottom',
+        'nameSpace':'45px',
+        'imgSrc':'../../../app/assets/images/product-images/green-eyed-bird.jpg'
+      },
+      {
+        'name':'Two Sun',
+        'type':'Earring',
+        'colorCategory':'Others',
+        'priceRange':'100-500',
+        'price':'Rs.250',
+        'bgPosition':'bottom',
+        'nameSpace':'45px',
+        'imgSrc':'../../../app/assets/images/product-images/two-sun.jpg'
+      },
+      {
+        'name':'Emerald Splash',
+        'type':'Earring',
+        'colorCategory':'Others',
+        'priceRange':'100-500',
+        'price':'Rs.250',
+        'bgPosition':'bottom',
+        'nameSpace':'45px',
+        'imgSrc':'../../../app/assets/images/product-images/emerald-splash.jpg'
+      },
+      {
+        'name':'Gothic Black',
+        'type':'Earring',
+        'colorCategory':'Others',
+        'priceRange':'100-500',
+        'price':'Rs.250',
+        'bgPosition':'bottom',
+        'nameSpace':'45px',
+        'imgSrc':'../../../app/assets/images/product-images/gothic-black.jpg'
       }
     ];
+    this.circled = false;
   }
+  // prodSelect() {
+  //   this.circled = true;
+  //   console.log('>>>',this.circled);
+  // }
 }
 
 export default ProddisplayController;
