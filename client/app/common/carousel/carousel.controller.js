7f895fa7c1ca850dd32b2206491e20e2661ff416
smanandhar11 <smananandhar@techdigitalcorp.com>
2017-09-05 19:23:27 -0500

    wip

--- client/app/common/carousel/carousel.controller.js
+++ client/app/common/carousel/carousel.controller.js
@@ -5,43 +5,68 @@ class CarouselController {
         activeOption: 0,
         options: [
           {
-            'imgSrc': '../../../app/resources/images/product-images/golden-crescent.jpg',
-            'name': 'Blue Dream',
+            'imgSrc': '../../../app/resources/images/carousel-images/nature1.jpg',
+            'name': 'Symmetrically Blissful',
           },
           {
-            'imgSrc':'../../../app/resources/images/product-images/turquoise-angel.jpg',
-            'name':'Turquoise Angel'
+            'imgSrc': '../../../app/resources/images/carousel-images/nature2.jpg',
+            'name': 'Colorful Emotion'
           },
           {
-            'imgSrc':'../../../app/resources/images/product-images/mars-burning.jpg',
-            'name':'Turquoise Angel'
+            'imgSrc': '../../../app/resources/images/carousel-images/nature3.jpg',
+            'name': 'Simple Times'
           },
           {
-            'imgSrc':'../../../app/resources/images/product-images/black-diamond.jpg',
-            'name':'2nd Last'
+            'imgSrc': '../../../app/resources/images/carousel-images/nature4.jpg',
+            'name': 'Revolutionary'
           },
           {
-            'imgSrc':'../../../app/resources/images/product-images/turquoise-angel.jpg',
-            'name':'Last'
+            'imgSrc': '../../../app/resources/images/carousel-images/nature5.jpg',
+            'name': 'Blue Moon'
           }
         ],
       },
     ];
+    this.showToggler = false;
+    this.toggleCarousel = false;
+  }
+
+  $onInit() {
+    if(this.toggleCarousel === true) {
+      this.next(x);
+    }
   }
 
   next(x) {
-    let noOfProds = x.options.length -1;
+    let noOfProds = x.options.length - 1;
     x.activeOption++;
-    if(x.activeOption > noOfProds) {
+    if (x.activeOption > noOfProds) {
       x.activeOption = 0;
     }
+
   }
+
   prev(x) {
-    let noOfProds = x.options.length -1;
+    let noOfProds = x.options.length - 1;
     x.activeOption--;
-    if(x.activeOption < 0) {
+    if (x.activeOption < 0) {
       x.activeOption = noOfProds;
     }
+  }
+
+  nextMouseOver() {
+    this.showToggler = true;
+    console.log(this.showToggler);
+  }
+
+  nextMouseLeave() {
+
+    console.log('hello');
+  }
+  caroToggle() {
+    console.log('started');
+    this.toggleCarousel = true;
+    console.log(this.toggleCarousel);
   }
 }
 
