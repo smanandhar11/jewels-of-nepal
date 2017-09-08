eb8877d210fc54329a738de6bbacd9401eabfc14
smanandhar11 <smananandhar@techdigitalcorp.com>
2017-09-08 00:52:58 -0500

    html css

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,13 +1,15 @@
 import angular from 'angular';
-import Home from './home/home';
 import About from './about/about';
 import Gallery from './gallery/gallery';
+import Home from './home/home';
+import Login from './login/login';
 import ProdDetails from './gallery/proddetails/proddetails'
 
 let componentModule = angular.module('app.components', [
   Home,
   About,
   Gallery,
+  Login,
   ProdDetails
 ])
 
