6ca7605e70f1c43b39b4ed2b72b77b259c813ba0
smanandhar11 <smananandhar@techdigitalcorp.com>
2017-09-12 21:57:48 -0500

    self

--- client/app/components/gallery/proddetails/proddetails.controller.js
+++ client/app/components/gallery/proddetails/proddetails.controller.js
@@ -1,17 +1,27 @@
-
+import _ from 'lodash';
 
 class ProddetailsController {
-  constructor($firebaseObject) {
+  constructor($firebaseObject, $stateParams) {
     'ngInject';
     this.$firebaseObject = $firebaseObject;
+    this.$stateParams = $stateParams;
   }
   $onInit() {
     const rootRef = firebase.database().ref().child('products');
     const ref = rootRef.child('blue-dream');
+    let self = this;
 
-    rootRef.on('value', function (snap) {
-      console.log(snap.val());
-    })
+    rootRef.on('value',(snap) =>  {
+      _.forEach(snap.val(), function (valu) {
+        // if(valu.id = self.$stateParams.id) {
+        //   console.log(valu.options.name);
+        // }
+        valu.id = self.$stateParams.id ? console.log(valu.options.name): console.log('false');
+        // console.log('value',valu.id);
+        // console.log('self',self.$stateParams.id);
+      })
+    });
+   /* console.log(this.$stateParams.id);*/
   }
 
 
