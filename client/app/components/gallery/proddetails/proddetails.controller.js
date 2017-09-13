3f5abd2b2827700ca08aa50b30b74185b13bb687
smanandhar11 <smananandhar@techdigitalcorp.com>
2017-09-13 12:23:51 -0500

    route param amost set

--- client/app/components/gallery/proddetails/proddetails.controller.js
+++ client/app/components/gallery/proddetails/proddetails.controller.js
@@ -1,5 +1,30 @@
+import _ from 'lodash';
+
 class ProddetailsController {
-  constructor() {this.name = 'proddetails';
+  constructor($firebaseObject, $stateParams) {
+    'ngInject';
+    this.$firebaseObject = $firebaseObject;
+    this.$stateParams = $stateParams;
+  }
+
+  $onInit() {
+    const rootRef = firebase.database().ref().child('products');
+    const ref = rootRef.child('blue-dream');
+    let self = this;
+
+    rootRef.on('value', (snap) => {
+      let fireData = (snap.val()); //settting as fireData to avoid .exp on fn
+
+
+      //todo: how to translate self to this to display on the template
+      _.forEach(fireData, function (val) {
+        let stParam = parseInt(self.$stateParams.id); // converting id fm string to number to evaluate
+        if(val.id === stParam) {
+          self.prodData = val;
+          console.log(self.prodData.options.name);
+        }
+      })
+    });
   }
 }
 
