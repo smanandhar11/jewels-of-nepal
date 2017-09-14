48d76d7c0e0ac1c7db957cf616fbc6b3c01c4e5e
smanandhar11 <smananandhar@techdigitalcorp.com>
2017-09-14 09:45:19 -0500

    js

--- client/app/components/login/login.controller.js
+++ client/app/components/login/login.controller.js
@@ -1,7 +1,49 @@
+import homeComponent from "../home/home.component";
+
 class LoginController {
-  constructor() {
-    this.name = 'login';
+  constructor($firebaseAuth, $state ,$firebaseObject) {
+    'ngInject';
+    this.$state = $state;
+    this.$firebaseAuth = $firebaseAuth;
+    this.$firebaseObject = $firebaseObject;
   }
+
+
+
+  $onInit() {
+    //firebase
+    const rootRef = firebase.database().ref().child('angular');
+    const ref = rootRef.child('object');
+    this.object = this.$firebaseObject(ref);
+
+    // ref.on('value', snap => this.objVal = snap.val());
+    //can also be written as
+    ref.on('value', function(snap) {
+      console.log(snap.val());
+    }, function(err) {
+      console.log(err);
+    })
+  }
+
+  signIn() {
+    let auth = this.$firebaseAuth();
+    let username = this.username;
+    let password = this.password;
+    self = this;
+
+    auth.$signInWithEmailAndPassword(username, password).then(function () {
+      //this is not being loaded into this function
+      self.$state.go('home');
+    }).catch(function (err) {
+      console.log(err);
+    });
+  }
+
+  move() {
+    // this.$state.go('home');
+    console.log('working');
+  }
+
 }
 
 export default LoginController;
