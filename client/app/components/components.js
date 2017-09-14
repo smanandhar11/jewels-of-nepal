66bfd73c0ce015bd7f32c97ba4cf09e93693ed95
smanandhar11 <smananandhar@techdigitalcorp.com>
2017-09-14 09:43:15 -0500

    init commit

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -3,12 +3,14 @@ import Home from './home/home';
 import About from './about/about';
 import Gallery from './gallery/gallery';
 import ProdDetails from './gallery/proddetails/proddetails'
+import Login from './login/login';
 
 let componentModule = angular.module('app.components', [
   Home,
   About,
   Gallery,
-  ProdDetails
+  ProdDetails,
+  Login
 ])
 
 .name;
